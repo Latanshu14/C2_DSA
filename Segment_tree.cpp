a834143c15cd9c01ecc489e162756ba785ba3b90
Latanshu Varshney <95296749+Latanshu14@users.noreply.github.com>
2022-10-29 01:26:42 +0530

    Update Segment_tree.cpp
    range sum query through segment tree

--- Segment_tree.cpp
+++ Segment_tree.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void createSegmentTree(vector<int> &a, vector<int> &segT, int low, int high, int posi){
+ 
+void createSegmentTree(vector<int> &a, vector<long long int> &segT, int low, int high, int posi){
 	if(low==high){
 		segT[posi] = a[low];
 		return; 
@@ -11,8 +11,8 @@ void createSegmentTree(vector<int> &a, vector<int> &segT, int low, int high, int
 	createSegmentTree(a, segT, mid+1, high, 2*posi+2);
 	segT[posi] += (segT[2*posi+1] + segT[2*posi+2]);
 }
-
-void changeValue(vector<int> &segT, vector<int> &a, int index, int value, int low, int high, int posi){
+ 
+void changeValue(vector<long long int> &segT, vector<int> &a, int index, int value, int low, int high, int posi){
 	if(index>=low && index<=high){
 		segT[posi] += (value - a[index]);
 		if(low==high && low==index){
@@ -24,8 +24,8 @@ void changeValue(vector<int> &segT, vector<int> &a, int index, int value, int lo
 	changeValue(segT, a, index, value, low, mid, 2*posi+1);
 	changeValue(segT, a, index, value, mid+1, high, 2*posi+2);
 }
-
-int rangeSumQuery(vector<int> &segT, int qlow, int qhigh, int low, int high, int posi){
+ 
+long long int rangeSumQuery(vector<long long int> &segT, int qlow, int qhigh, int low, int high, int posi){
 	// TOTAL OVERLAP
 	if(low>=qlow && high<=qhigh){
 		return segT[posi];
@@ -37,9 +37,9 @@ int rangeSumQuery(vector<int> &segT, int qlow, int qhigh, int low, int high, int
 	int mid = (low+high)/2;
 	return (rangeSumQuery(segT, qlow, qhigh, low, mid, 2*posi+1) + rangeSumQuery(segT, qlow, qhigh, mid+1, high, 2*posi+2));
 }
-
+ 
 void solve(){
-	int n; cin>>n;
+	int n, q; cin>>n>>q;
 	vector<int> a(n);
 	for(int i=0; i<n; i++){
 		cin>>a[i];
@@ -54,27 +54,28 @@ void solve(){
 	else{
 		sz = 4*n-1;
 	}
-	vector<int> segT(sz, 0);
+	vector<long long int> segT(sz, 0);
 	createSegmentTree(a, segT, 0, n-1, 0);
-	for(int i=0; i<sz; i++){
-		cout<<segT[i]<<" ";
-	}
-	cout<<"\n";
-	int q; cin>>q;
 	while(q--){
-		int idx, val, low, high;
-		cin>>idx>>val>>low>>high;
-		if(idx>=low && idx<=high){
-			changeValue(segT, a, idx, val, 0, n-1, 0);
+		int type; cin>>type;
+		if(type==1){
+			int k, u; cin>>k>>u;
+			changeValue(segT, a, k-1, u, 0, n-1, 0);
+			a[k-1] = u;
 		}
-		cout<<rangeSumQuery(segT, low, high, 0, n-1, 0)<<"\n";
+		else{
+			int low, high;
+			cin>>low>>high;
+			cout<<rangeSumQuery(segT, low-1, high-1, 0, n-1, 0)<<"\n";
+		}
 	}
 }
-
+ 
 int main(){
-	int t; cin>>t;
+	solve();
+	/*int t; cin>>t;
 	while(t--){
 		solve();
 	}
-	return 0;
+*/	return 0;
 }
